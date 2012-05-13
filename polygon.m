207d4bd5f70202491162c0d9e28914fb223ce2b1
Simeon Simeonov <sss41@cam.ac.uk>
2012-05-13 20:20:29 +0100

    Transformed polygon.m into a function (was a script)

--- polygon.m
+++ polygon.m
@@ -1,20 +1,14 @@
-clear all;
-close all;
-clc;
+function y = polygon(x)
 
-
-N = 10; % Number of points
-maxInt = 100; % Max coordinate
-
-x = randi(maxInt, N, 2); % Get random integer coordinates within interval [0, maxInt]
-
-ind = zeros(max(size(x)), 1); % ind contains the indices of the sorted coordinates
+N = size(x,1); % Number of points
+ind = zeros(N, 1); % ind contains the indices of the sorted coordinates
+y = zeros(N+1,2);
 p = 1; % pointer for the ind matrix
 
-% a contains the index of the starting point - the point with min y coordinate. A
-% special case is that several points might have the same min y coordinate.
-% If this happens we want to choose the point with min x coordinate amongst
-% them.
+% a contains the index(ices) of the starting point(s) - the point(s) with
+% min y coordinate(s). A special case is that several points might have the
+% same min y coordinate(s). If this happens we want to choose the point
+% with min x coordinate amongst them.
 a = find(x(:,2)==min(x(:,2)));
 
 % Sort the points with min y coordinate wrt increasing x coordinate. This
@@ -27,17 +21,12 @@ ind(p:size(b,1)) = i;
 
 p = p + size(a,1);
 
-d = x - repmat(x(ind(1),:), max(size(x)), 1);
-th_tmp = d(:,2)./(d(:,1) + d(:,2));
+d = x - repmat(x(ind(1),:), N, 1);
+th = d(:,2)./(d(:,1) + d(:,2));
 
-th = {th_tmp(th_tmp>0) th_tmp(th_tmp<0)};
-t = [sort(th{1}); sort(th{2})];
-[v i] = ismember(t, th_tmp);
+t = [sort(th(th>0)); sort(th(th<0))];
+[v i] = ismember(t, th);
 ind = [ind(ind~=0); i];
 
 y = x(ind,:);
 y(N+1, :) = y(1,:);
-
-plot(y(:,1), y(:,2), '*');
-hold on;
-plot(y(:,1), y(:,2));
