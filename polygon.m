96066936dc53f45c969ed34b36e320afa039bb6e
Simeon Simeonov <sss41@cam.ac.uk>
2012-05-13 22:29:09 +0100

    Fixed a bug with initial values

--- polygon.m
+++ polygon.m
@@ -3,7 +3,6 @@ function y = polygon(x)
 N = size(x,1); % Number of points
 ind = zeros(N, 1); % ind contains the indices of the sorted coordinates
 y = zeros(N+1,2);
-p = 1; % pointer for the ind matrix
 
 % a contains the index(ices) of the starting point(s) - the point(s) with
 % min y coordinate(s). A special case is that several points might have the
@@ -14,12 +13,8 @@ a = find(x(:,2)==min(x(:,2)));
 % Sort the points with min y coordinate wrt increasing x coordinate. This
 % allows us to have the starting point at the index 1 in the b matrix and
 % the rest of b contains the first few points of the polygon.
-b = sort(x(a,1));
-
-[v i] = ismember(b, x(:, 1));
-ind(p:size(b,1)) = i;
-
-p = p + size(a,1);
+[v i] = sort(x(a,1));
+ind(1:size(a,1)) = a(i);
 
 d = x - repmat(x(ind(1),:), N, 1);
 th = d(:,2)./(d(:,1) + d(:,2));
